e9e4cc5bf3e6dd1f98080f329f11f545620d3051
bhernez <bhernez@users.noreply.github.com>
2018-07-22 14:02:06 -0500

    Ensure collection name is correct for Firestore
    Replace slashes inside buildCollectionName to something compatible with Firestore

--- addon/utils/parser.js
+++ addon/utils/parser.js
@@ -68,7 +68,7 @@ export function parseDocSnapshot(type, docSnapshot) {
  * @function
  */
 export function buildCollectionName(name) {
-  return camelize(pluralize(`${name}`.replace('/', '-')));
+  return camelize(pluralize(`${name}`)).replace('/', '-');
 }
 
 /**
